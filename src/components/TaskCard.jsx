89b1d02fa420080fe4a2a4e51b4c9685db2a1745
aguerolas <sebaguero8@gmail.com>
2022-10-11 02:46:15 -0500

    second commit :0

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -8,8 +8,8 @@ const TaskCard = ({task}) => {
 
   if(!task.title && !task.description) return null
   return (
-      <div className="  bg-gray-800 text-white p-5 rounded-2xl " >
-        <h1 className="text-xl font-bold capitalize" >{task.title}</h1>
+      <div className="  bg-gray-800 text-white p-5 rounded-md " >
+        <h1 className="text-md text-center font-bold capitalize" >{task.title}</h1>
         <p className="text-gray-400 text-sm" >{task.description}</p>
         <button className="bg-blue-500 px-2 py-1 rounded-md mt-4 hover:text-lg hover:bg-red-400 hover:font-bold" onClick={()=> deleteItem (task.id) } >Delite item </button>
       </div>
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -9,7 +9,7 @@ const { tasks} = useContext(TaskContext)
   if(tasks.length===0) return <h1 className='text-white text-8xl font-bold text-center' >no hay tareas</h1>
 
 return (
-    <div className='  grid grid-cols-4 gap-3' >
+    <div className=' grid grid-cols-2 md:grid-cols-4 gap-3 flex-wrap' >
         {tasks.map((task) => ( 
           <TaskCard  key={task.id} task= {task}  /> 
      
